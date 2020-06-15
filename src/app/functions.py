9e2322205c6fc14324578f5f53dd31520fd7fe42
Michał <miczur99@o2.pl>
2020-06-15 22:51:57 +0200

    Dodawanie firmy

--- src/app/functions.py
+++ src/app/functions.py
@@ -6,3 +6,13 @@ def rejestracja_uzytkownika():
     nazwisko = input("Podaj imie: ")
 
     operations.dodaj_uzytkownika(imie, nazwisko)
+
+def dodawanie_firmy():
+    nazwa = input("Podaj nazwe firmy: ")
+    branza = input("Podaj branze: ")
+    miasto = input("Podaj miasto, w ktorym sie znajduje: ")
+    liczba_opinii = "0"
+    srednia = "0"
+
+
+    operations.dodaj_firme(nazwa, branza, miasto, liczba_opinii, srednia)
--- src/app/program.py
+++ src/app/program.py
@@ -11,8 +11,23 @@ def menu():
     print(("Co chcesz teraz zrobić? "))
     print("0 - wyjscie z programu")
     print("1 - obsluga uzytkownika")
+    print("2 - obsluga firmy")
     temp = input("Wpisz cyfre: ")
 
+    if temp == "2":
+        print(("Co chcesz teraz zrobić? "))
+        print("0 - wyjscie z programu")
+        print("1 - dodawanie firmy")
+        temp = input("Wpisz cyfre: ")
+
+        if temp == "1":
+            functions.dodawanie_firmy()
+            menu()
+        elif temp == "0":
+            sys.exit(0)
+        else:
+            print("bledny wybor")
+            menu()
 
     if temp == "1":
         print(("Co chcesz teraz zrobić? "))
