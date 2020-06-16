3da6d5d73689b15fa80758ccffec8e316aa19a29
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2020-06-16 22:07:06 +0200

    update sql

--- src/sql/trustnet (3).sql
+++ src/sql/trustnet (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 15, 2020 at 12:37 PM
+-- Generation Time: Jun 16, 2020 at 10:05 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
@@ -36,6 +36,16 @@ CREATE TABLE `firma` (
   `srednia` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
+--
+-- Dumping data for table `firma`
+--
+
+INSERT INTO `firma` (`id_firma`, `nazwa`, `branza`, `miasto`, `liczba_opinii`, `srednia`) VALUES
+(1, 'jan grzegorz trzeci glowice', 'janice pawlice', 'janów lubelski', 0, 0),
+(2, 'Janek Podolski uslugi', 'Fryzjer', 'Krakow', 0, 0),
+(4, 'Hej sokoly firma handlowa', 'Gastronomia', 'Krakow', 0, 0),
+(5, 'Fryzjerstwo u mnie i Spolki', 'Fryzjer', 'Krakow', 0, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -46,8 +56,21 @@ CREATE TABLE `opinie` (
   `id_opinia` int(11) NOT NULL,
   `liczba_gwiazdek` int(11) NOT NULL,
   `opis` text COLLATE utf8_polish_ci NOT NULL,
-  `FirmaID` int(11) NOT NULL
+  `FirmaID` int(11) NOT NULL,
+  `UzytkownikID` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Dumping data for table `opinie`
+--
+
+INSERT INTO `opinie` (`id_opinia`, `liczba_gwiazdek`, `opis`, `FirmaID`, `UzytkownikID`) VALUES
+(1, 3, 'dsd', 2, 1),
+(2, 5, 'Kuku', 4, 1),
+(3, 2, 'spoko firma', 4, 1),
+(4, 2, 'spoko firma ale moja curka studiuje prawo', 5, 1),
+(5, 1, 'fajnie i spoko', 5, 1),
+(6, 3, 'fajna firma i wogule', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -118,13 +141,13 @@ ALTER TABLE `uzytkownik`
 -- AUTO_INCREMENT for table `firma`
 --
 ALTER TABLE `firma`
-  MODIFY `id_firma` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_firma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `opinie`
 --
 ALTER TABLE `opinie`
-  MODIFY `id_opinia` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_opinia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `uzytkownik`
